c96cbc4d519f29ccc5a9f46eb8f6cceb85577184
Prateek <prateek.p@newstreettech.com>
2024-01-09 17:04:23 +0530

    Clean app state

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -1,6 +1,9 @@
+import { useAppState } from '@/store/useAppState';
 import React from 'react';
 
 const Stats = () => {
+	const app = useAppState();
+	console.log(app);
 	return <div>Stats</div>;
 };
 
