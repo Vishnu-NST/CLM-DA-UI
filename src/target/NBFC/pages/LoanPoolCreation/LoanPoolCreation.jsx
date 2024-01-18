149494a227d3a1a4c0f5bb1633e899306f2eaafc
kalai-malar-d <kalaimalar.d@newstreettech.com>
2024-01-18 15:03:51 +0530

    adding breadcrumbs in header

--- src/target/NBFC/pages/LoanPoolCreation/LoanPoolCreation.jsx
+++ src/target/NBFC/pages/LoanPoolCreation/LoanPoolCreation.jsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { Box, Grid, Tab, Tabs, Typography } from '@mui/material';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import SelectComponent from '../../../../components/SelectComponent';
@@ -31,6 +31,35 @@ import React from 'react';
 import DataIntegration from './DataIntegration';
 import ReviewPool from './ReviewPool';
 import HeaderComp from '@/components/HeaderComponent';
+import { ViewLoanPool } from '../ViewLoanPool/ViewLoanPool';
+import SearchIcon from '../../../../assets/svg/SearchIcon';
+
+function CustomTabPanel(props) {
+	const { children, value, index, ...other } = props;
+
+	return (
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+			{...other}
+		>
+			{value === index && (
+				<Box sx={{ pt: 5, pb: 3 }}>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
+		</div>
+	);
+}
+
+function a11yProps(index) {
+	return {
+		id: `simple-tab-${index}`,
+		'aria-controls': `simple-tabpanel-${index}`,
+	};
+}
 
 const customButtonStyle = {
 	borderRadius: '7px',
@@ -66,6 +95,9 @@ const validationSchema = yup.object({
 });
 
 const LoanPoolCreation = () => {
+	const [value, setValue] = React.useState(0);
+	const [searchVal, setSearchVal] = React.useState('');
+
 	const breadCrumbs = [
 		{
 			data: 'add Product portfolio',
@@ -76,9 +108,37 @@ const LoanPoolCreation = () => {
 			path: null,
 		},
 	];
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
+
 	const [showForm, setShowForm] = React.useState(true);
 	const [showDataIntegration, setShowDataIntegration] = React.useState(false);
 	const [showReview, setShowReview] = React.useState(false);
+
+	const searchAttributes = {
+		id: 'searchVal',
+		name: 'searchVal',
+		placeholder: `Search`,
+		type: 'text',
+		value: searchVal,
+		onChange: (event) => {
+			console.log({ event });
+			setSearchVal(event.target.value);
+		},
+		onkeydown: (evt) => {
+			(evt.key === '+' ||
+				evt.key === '-' ||
+				evt.key === '.' ||
+				evt.key === 'e' ||
+				evt.key === 'ArrowUp' ||
+				evt.key === 'ArrowDown') &&
+				evt.preventDefault();
+		},
+		disabled: false,
+		endIcon: <SearchIcon />,
+	};
 
 	const formik = useFormik({
 		initialValues: {
@@ -109,135 +169,219 @@ const LoanPoolCreation = () => {
 		},
 	});
 
+	const TabComponent = () => {
+		return (
+			<>
+				<Box
+					sx={{
+						paddingTop: '0.5rem',
+						borderBottom: 1,
+						borderColor: 'rgba(112, 126, 174, 0.2)',
+						width: 'fit-content',
+					}}
+				>
+					<Tabs
+						value={value}
+						onChange={handleChange}
+						aria-label="basic tabs example"
+						TabIndicatorProps={{
+							style: {
+								backgroundColor: '#C4161C',
+							},
+						}}
+						sx={{
+							'& .MuiTab-root.Mui-selected': {
+								color: '#C4161C',
+								fontFamily: 'Poppins, sans-serif',
+								fontWeight: '600',
+								fontSize: '0.8rem',
+							},
+							fontFamily: 'Poppins, sans-serif',
+							fontSize: '0.8rem',
+							fontWeight: '600',
+							color: '#8794C2',
+						}}
+					>
+						<Tab
+							label="Create Loan Pool"
+							sx={{
+								paddingBottom: '0rem',
+								paddingLeft: '0rem',
+								fontSize: '0.8rem',
+								fontFamily: 'Poppins, sans-serif',
+							}}
+							{...a11yProps(0)}
+						/>
+						<Tab
+							label="View loan Pool"
+							sx={{
+								paddingBottom: '0rem',
+								fontSize: '0.8rem',
+								fontFamily: 'Poppins, sans-serif',
+							}}
+							{...a11yProps(1)}
+						/>
+					</Tabs>
+				</Box>
+			</>
+		);
+	};
+
+	const SearchComp = () => {
+		return value === 1 ? (
+			<>
+				<InputComponent {...searchAttributes} mbVal={'0rem'} />
+			</>
+		) : null;
+	};
+
 	return (
 		<>
-			<HeaderComp title={'Loan Pool Creation'} breadCrumbs={breadCrumbs} />
-			{showForm && (
-				<div className="card-block">
-					<div className="loan-pool-form-block">
-						<div className="title">Enter Loan Pool Info</div>
-						<form onSubmit={formik.handleSubmit}>
-							<div className="sub-title">Basic Info</div>
-							<Grid container>
-								<Grid item sm={4} className="pr-2">
-									<SelectComponent
-										{...productAttributes(formik)}
-									/>
+			<HeaderComp
+				title={'Loan Pool Creation'}
+				breadCrumbs={breadCrumbs}
+				TabsComp={TabComponent}
+				SearchComp={SearchComp}
+			/>
+			<CustomTabPanel value={value} index={0}>
+				{showForm && (
+					<div className="card-block">
+						<div className="loan-pool-form-block">
+							<div className="title">Enter Loan Pool Info</div>
+							<form onSubmit={formik.handleSubmit}>
+								<div className="sub-title">Basic Info</div>
+								<Grid container>
+									<Grid item sm={4} className="pr-2">
+										<SelectComponent
+											{...productAttributes(formik)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<DatePickerComponent
+											{...closureDateAttributes(formik)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<SelectComponent
+											{...creditRatingAttributes(formik)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent {...aumAttributes(formik)} />
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent
+											{...averageIrrAttributes(formik)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent
+											{...averageBalanceTenorAttributes(
+												formik,
+											)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent
+											{...averageLoanAmtAttributes(formik)}
+										/>
+									</Grid>
 								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<DatePickerComponent
-										{...closureDateAttributes(formik)}
-									/>
+								<div className="sub-title">Additional Info</div>
+								<Grid container>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent
+											{...par01PlusAttributes(formik)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent
+											{...par30Attributes(formik)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent
+											{...par60Attributes(formik)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent
+											{...par90Attributes(formik)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent
+											{...par90PlusAttributes(formik)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent
+											{...nbfcCoveredAttributes(formik)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent
+											{...statesCoveredAttributes(formik)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent
+											{...districtsCoveredAttributes(formik)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent
+											{...loansCoveredAttributes(formik)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent
+											{...firstCycleLoansAttributes(formik)}
+										/>
+									</Grid>
+									<Grid item sm={4} className="pr-2">
+										<InputComponent
+											{...secondCycleLoansAttributes(formik)}
+										/>
+									</Grid>
 								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<SelectComponent
-										{...creditRatingAttributes(formik)}
-									/>
-								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent {...aumAttributes(formik)} />
-								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent
-										{...averageIrrAttributes(formik)}
-									/>
-								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent
-										{...averageBalanceTenorAttributes(formik)}
-									/>
-								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent
-										{...averageLoanAmtAttributes(formik)}
-									/>
-								</Grid>
-							</Grid>
-							<div className="sub-title">Additional Info</div>
-							<Grid container>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent
-										{...par01PlusAttributes(formik)}
-									/>
-								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent {...par30Attributes(formik)} />
-								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent {...par60Attributes(formik)} />
-								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent {...par90Attributes(formik)} />
-								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent
-										{...par90PlusAttributes(formik)}
-									/>
-								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent
-										{...nbfcCoveredAttributes(formik)}
-									/>
-								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent
-										{...statesCoveredAttributes(formik)}
-									/>
-								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent
-										{...districtsCoveredAttributes(formik)}
-									/>
-								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent
-										{...loansCoveredAttributes(formik)}
-									/>
-								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent
-										{...firstCycleLoansAttributes(formik)}
-									/>
-								</Grid>
-								<Grid item sm={4} className="pr-2">
-									<InputComponent
-										{...secondCycleLoansAttributes(formik)}
-									/>
-								</Grid>
-							</Grid>
-							<Grid container>
-								<Grid
-									item
-									sm={12}
-									sx={{
-										display: 'flex',
-										alignItems: 'center',
-										justifyContent: 'flex-end',
-									}}
-								>
-									<CustomButton
-										buttonType={'submit'}
-										buttonDisabled={false}
-										customStyle={customButtonStyle}
+								<Grid container>
+									<Grid
+										item
+										sm={12}
+										sx={{
+											display: 'flex',
+											alignItems: 'center',
+											justifyContent: 'flex-end',
+										}}
 									>
-										Next &nbsp;
-										<ArrowForwardIosIcon fontSize="11" />
-									</CustomButton>
+										<CustomButton
+											buttonType={'submit'}
+											buttonDisabled={false}
+											customStyle={customButtonStyle}
+										>
+											Next &nbsp;
+											<ArrowForwardIosIcon fontSize="11" />
+										</CustomButton>
+									</Grid>
 								</Grid>
-							</Grid>
-						</form>
+							</form>
+						</div>
 					</div>
-				</div>
-			)}
-			{showDataIntegration && (
-				<DataIntegration
-					onSubmit={() => {
-						setShowDataIntegration(false);
-						setShowReview(true);
-					}}
-				/>
-			)}
-			{showReview && <ReviewPool />}
+				)}
+				{showDataIntegration && (
+					<DataIntegration
+						onSubmit={() => {
+							setShowDataIntegration(false);
+							setShowReview(true);
+						}}
+					/>
+				)}
+				{showReview && <ReviewPool />}
+			</CustomTabPanel>
+			<CustomTabPanel value={value} index={1}>
+				<ViewLoanPool />
+			</CustomTabPanel>
 		</>
 	);
 };
--- src/target/NBFC/pages/ViewLoanPool/ViewLoanPool.jsx
+++ src/target/NBFC/pages/ViewLoanPool/ViewLoanPool.jsx
@@ -1,11 +1,10 @@
-import LoanPoolCard from "./components/LoanPoolCard"
+import LoanPoolCard from './components/LoanPoolCard';
 
 export const ViewLoanPool = () => {
-
-    return (
-            <div align="center" style={{backgroundColor:"#F8F9FB", height:"100vh"}}>
-                <br />
-                <LoanPoolCard />
-            </div>
-    )
-}
+	return (
+		<div style={{ backgroundColor: '#F8F9FB', height: '100vh' }}>
+			<br />
+			<LoanPoolCard />
+		</div>
+	);
+};
