c932820ea6ccd8b323814b4e4d28487f4010c582
Manish Easwar <manishe@newstreettech.com>
2024-01-22 13:55:56 +0530

    Loan pool delete dialog initiated

---
+++ src/assets/svg/DeletedSuccessfullyIcon.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const DeletedSuccessfullyIcon = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="125"
+			height="124"
+			viewBox="0 0 125 124"
+			fill="none"
+		>
+			<path
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+				d="M0.5 62C0.5 45.5566 7.03212 29.7866 18.6594 18.1594C30.2866 6.53212 46.0566 0 62.5 0C78.9434 0 94.7134 6.53212 106.341 18.1594C117.968 29.7866 124.5 45.5566 124.5 62C124.5 78.4434 117.968 94.2134 106.341 105.841C94.7134 117.468 78.9434 124 62.5 124C46.0566 124 30.2866 117.468 18.6594 105.841C7.03212 94.2134 0.5 78.4434 0.5 62ZM58.9619 88.536L94.6573 43.9125L88.2093 38.7541L57.7715 76.7891L36.212 58.8256L30.9213 65.1744L58.9619 88.5443V88.536Z"
+				fill="url(#paint0_linear_717_29675)"
+			/>
+			<defs>
+				<linearGradient
+					id="paint0_linear_717_29675"
+					x1="62.5"
+					y1="-142.26"
+					x2="62.5"
+					y2="124"
+					gradientUnits="userSpaceOnUse"
+				>
+					<stop stop-color="#00B85E" />
+					<stop offset="1" stop-color="#00B85E" stop-opacity="0" />
+				</linearGradient>
+			</defs>
+		</svg>
+	);
+};
+
+export default DeletedSuccessfullyIcon;
